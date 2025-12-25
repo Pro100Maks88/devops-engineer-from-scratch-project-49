3cd518068eae1c4ef39729a35f81ecedb8518436
Pro100Maks88 <pro100maks88@yandex.ru>
2025-12-25 16:07:03 +0300

    update even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,14 +1,9 @@
-DESCRIPT = "Answer 'yes' if the number is even, otherwise answer 'no'."
+DESCRIPT = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 import random
 
 def generate_question():
-
     number = random.randint(1, 100)
     question = str(number)
     correct_answer = "yes" if number % 2 == 0 else "no"
     return question, correct_answer
-
-'''if __name__ == "__main__":
-    from brain_games.engine import play_game
-    play_game(__name__)'''

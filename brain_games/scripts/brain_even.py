2ec2138a8a64901ebfd6af12102919228633293d
Pro100Maks88 <pro100maks88@yandex.ru>
2025-12-25 15:52:20 +0300

    update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,6 +1,9 @@
+from brain_games.engine import play_game
+from brain_games.games import even 
+
+
 def main():
-    from brain_games.engine import play_game
-    play_game("brain_games.games.even")
+    play_game(even)
 
 
 if __name__ == "__main__":
